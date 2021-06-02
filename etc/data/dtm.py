7d42cfd774ad8cf1589d119d52a810613b6e4c78
dannielgloria <danniel.gloria@gmail.com>
2021-06-02 14:37:38 -0500

    fixed

--- etc/data/dtm.py
+++ etc/data/dtm.py
@@ -34,7 +34,7 @@ def getDataSet():
 def dataAyun():
     #     os.remove('./etc/data/files/.DS_Store')
     content = os.listdir('./etc/data/files')
-    df = pd.read_csv('./etc/data/files/'+content[1]+'/CAMP_AYUN_2021.csv', header=None, sep='\n')
+    df = pd.read_csv('./etc/data/files/'+content[0]+'/CAMP_AYUN_2021.csv', header=None, sep='\n')
     df = df[0].str.split(',', expand=True)
     df = df.drop(df.index[[0,1,2,3,4]])
     total = df[34].sum()
@@ -86,7 +86,7 @@ def dataDip():
 
 def dataGob():
     content = os.listdir('./etc/data/files')
-    df = pd.read_csv('./etc/data/files/'+content[1]+'/CAMP_GOB_2021.csv', header=None, sep='\n')
+    df = pd.read_csv('./etc/data/files/'+content[2]+'/CAMP_GOB_2021.csv', header=None, sep='\n')
     df = df[0].str.split(',', expand=True)
     df = df.drop(df.index[[0,1,2,3,4]])
     total = df[34].sum()
@@ -112,7 +112,7 @@ def dataGob():
 
 def dataGobCand():
     content = os.listdir('./etc/data/files')
-    df = pd.read_csv('./etc/data/files/'+content[1]+'/CAMP_GOB_CANDIDATURA_2021.csv')
+    df = pd.read_csv('./etc/data/files/'+content[2]+'/CAMP_GOB_CANDIDATURA_2021.csv')
     df = df.drop(columns=['ID_ESTADO'])
     df = df.set_index('PARTIDO_CI')
     result = df.to_json()
@@ -120,7 +120,7 @@ def dataGobCand():
     
 def dataJuntas():
     content = os.listdir('./etc/data/files')
-    df = pd.read_csv('./etc/data/files/'+content[2]+'/CAMP_JUNTAS_2021.csv', header=None, sep='\n')
+    df = pd.read_csv('./etc/data/files/'+content[3]+'/CAMP_JUNTAS_2021.csv', header=None, sep='\n')
     df = df[0].str.split(',', expand=True)
     df = df.drop(df.index[[0,1,2,3,4]])
     total = df[33].sum()
