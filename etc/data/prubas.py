90f5317cfbec117e0011f476299200a863c96e79
dannielgloria <danniel.gloria@gmail.com>
2021-06-02 16:37:10 -0500

    ok

--- etc/data/prubas.py
+++ etc/data/prubas.py
@@ -10,8 +10,13 @@ from pytz import timezone
 df = pd.read_csv('./files/20210601_1852_PREP_JUNTAS_CAMP/CAMP_JUNTAS_2021.csv', header=None, sep='\n')
 df = df[0].str.split(',', expand=True)
 df = df.drop(df.index[[0,1,2,3,4,5]])
+df = df.reset_index()
+
+print (df)
 total = df[35].sum()
-pan = df[int(16)].sum()
+pan = pd.to_numeric(df[16]).sum()
+# pan = pan.sum()
+print (pan)
 pri = df[17].sum()
 prd = df[18].sum()
 pt = df[19].sum()
@@ -26,10 +31,10 @@ pan_pri = df[27].sum()
 pan_prd = df[28].sum()
 pri_prd = df[29].sum()
 pt_mor = df[30].sum()
-data = { "PAN":pan,"PRI":pri,"PRD":prd,"PT":pt,"PVEM":pvem,"MC":mc,"MOR":mor,"PES":pes,"RSP":rsp,"FM":fm,"C_PAN_PRI_PRD": pan_pri_prd, "C_PAN_PRI":pan_pri,"C_PAN_PRD": pan_prd, "C_PRI_PRD": pri_prd, "C_PT_MOR": pt_mor }
-result = json.dumps(data)
+# data = { "PAN":pan,"PRI":pri,"PRD":prd,"PT":pt,"PVEM":pvem,"MC":mc,"MOR":mor,"PES":pes,"RSP":rsp,"FM":fm,"C_PAN_PRI_PRD": pan_pri_prd, "C_PAN_PRI":pan_pri,"C_PAN_PRD": pan_prd, "C_PRI_PRD": pri_prd, "C_PT_MOR": pt_mor }
+# result = json.dumps(data)
 # da = df.drop(columns=[0])
-print (result)
+# print (result)
 # df = df.drop(columns=['ID_ESTADO'])
 # df = df.set_index('PARTIDO_CI')
 # da = df.T
