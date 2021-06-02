98c154cdc770923ef462577b3383e9228fa421ea
dannielgloria <danniel.gloria@gmail.com>
2021-06-02 13:51:15 -0500

    funciona

--- app.py
+++ app.py
@@ -1,4 +1,49 @@
-from etc.app import app
+import re
+import time
+import json
+from flask import Flask, request, flash, jsonify
+from etc.data.dtm import dataAyun, dataDip, dataGob, dataGobCand, dataJuntas, removeDS_Store
+# from data.dtm import dataAyun, dataDip, dataGob, dataGobCand, dataJuntas
+
+# FLASK Instance my application
+app = Flask(__name__)
+
+@app.route('/datosAyuntamiento',methods=['GET'])
+def get_dataAyun():
+    resp = dataAyun()
+    resp.status_code=200
+    return resp
+
+@app.route('/datosDiputaciones',methods=['GET'])
+def get_dataDiputaciones():
+    resp = dataDip()
+    resp.status_code=200
+    return resp
+
+@app.route('/datosGobernatura',methods=['GET'])
+def get_dataGob():
+    resp = dataGob()
+    resp.status_code=200
+    return resp
+
+@app.route('/candidatosGobernatura',methods=['GET'])
+def get_dataGobCand():
+    resp = dataGobCand()
+    resp.status_code=200
+    return resp
+
+@app.route('/datosJuntas',methods=['GET'])
+def get_dataJuntas():
+    resp = dataJuntas()
+    resp.status_code=200
+    return resp
+
+@app.route('/desatoraDatos',methods=['GET'])
+def get_desatoraDatos():
+    removeDS_Store()
+    resp = 'DS_Store deleted'
+    resp.status_code=200
+    return resp
 
 DEBUG = False
 PORT = 5000
