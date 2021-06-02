896b2d7c4a25aac2fe7f1ead8bd0a3560d615307
dannielgloria <danniel.gloria@gmail.com>
2021-06-02 18:53:45 -0500

    Flask-CORS

--- etc/app.py
+++ etc/app.py
@@ -1,50 +1,59 @@
 import re
 import time
 import json
+from flask_cors import CORS
 from flask import Flask, request, flash, jsonify
 from etc.data.dtm import dataAyun, dataDip, dataGob, dataGobCand, dataJuntas, removeDS_Store, direcciones
 # from data.dtm import dataAyun, dataDip, dataGob, dataGobCand, dataJuntas
 
 # FLASK Instance my application
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/datosAyuntamiento',methods=['GET'])
+@cross_origin()
 def get_dataAyun():
     resp = dataAyun()
     resp = json.loads(resp)
     return resp
 
 @app.route('/datosDiputaciones',methods=['GET'])
+@cross_origin()
 def get_dataDiputaciones():
     resp = dataDip()
     resp = json.loads(resp)
     return resp
 
 @app.route('/datosGobernatura',methods=['GET'])
+@cross_origin()
 def get_dataGob():
     resp = dataGob()
     resp = json.loads(resp)
     return resp
 
 @app.route('/candidatosGobernatura',methods=['GET'])
+@cross_origin()
 def get_dataGobCand():
     resp = dataGobCand()
     resp = json.loads(resp)
     return resp
 
 @app.route('/datosJuntas',methods=['GET'])
+@cross_origin()
 def get_dataJuntas():
     resp = dataJuntas()
     resp = json.loads(resp)
     return resp
 
 @app.route('/desatoraDatos',methods=['GET'])
+@cross_origin()
 def get_desatoraDatos():
     removeDS_Store()
     resp = 'DS_Store deleted'
     return resp
 
 @app.route('/directorios',methods=['GET'])
+@cross_origin()
 def get_direcciones():
     resp = str(direcciones())
     # resp.status_code=200
