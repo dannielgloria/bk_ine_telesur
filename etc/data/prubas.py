711130060116f29409364fdfb8ae8e290499e6b4
dannielgloria <danniel.gloria@gmail.com>
2021-06-04 17:12:43 -0500

    datAyun

--- etc/data/prubas.py
+++ etc/data/prubas.py
@@ -7,45 +7,51 @@ import pandas as pd
 from shutil import rmtree
 from pytz import timezone
 
-df = pd.read_csv('./files/20210601_1852_PREP_GOB_CAMP/CAMP_GOB_2021.csv', header=None, sep='\n')
+df = pd.read_csv('./files/20210601_1852_PREP_AYUN_CAMP/CAMP_AYUN_2021.csv', header=None, sep='\n')
 df = df[0].str.split(',', expand=True)
 df = df.drop(df.index[[0,1,2,3,4,5]])
-total = pd.to_numeric(df[34]).sum()
-pan = pd.to_numeric(df[16]).sum()
-pri = pd.to_numeric(df[17]).sum()
-prd = pd.to_numeric(df[18]).sum()
-pt = pd.to_numeric(df[19]).sum()
-pvem = pd.to_numeric(df[20]).sum()
-mc = pd.to_numeric(df[21]).sum()
-mor = pd.to_numeric(df[22]).sum()
-pes = pd.to_numeric(df[23]).sum()
-rsp = pd.to_numeric(df[24]).sum()
-fm = pd.to_numeric(df[25]).sum()
-pan_pri_prd = pd.to_numeric(df[26]).sum()
-pan_pri = pd.to_numeric(df[27]).sum()
-pan_prd = pd.to_numeric(df[28]).sum()
-pri_prd = pd.to_numeric(df[29]).sum()
-pt_mor = pd.to_numeric(df[30]).sum()
-no_reg = pd.to_numeric(df[31]).sum()
-panP = (pan*100)/total
-priP = (pri*100)/total
-prdP = (prd*100)/total
-ptP = (pt*100)/total
-pvemP = (pvem*100)/total
-mcP = (mc*100)/total
-morP = (mor*100)/total
-pesP = (pes*100)/total
-rspP = (rsp*100)/total
-fmP = (fm*100)/total
-pan_pri_prdP = (pan_pri_prd*100)/total
-pan_priP = (pan_pri*100)/total
-pan_prdP = (pan_prd*100)/total
-pri_prdP = (pri_prd*100)/total
-pt_morP = (pt_mor*100)/total
-no_regP = (no_reg*100)/total
-print(total)
-data = { "PAN":{"Name":"none","votes":pan,"percent":panP},"PRI":{"Name":"none","votes":pri,"percent":priP},"PRD":{"Name":"none","votes":prd,"percent":prdP},"PT":{"Name":"none","votes":pt,"percent":ptP},"PVEM":{"Name":"none","votes":pvem,"percent":pvemP},"MC":{"Name":"ELISEO FERNANDEZ MONTUFAR","votes":mc,"percent":mcP},"MOR":{"Name":"none","votes":mor,"percent":morP},"PES":{"Name":"NIC-THE-HA AGUILERA SILVA","votes":pes,"percent":pesP},"RSP":{"Name":"MARIA MAGDALENA COCOM ARBEZ","votes":rsp,"percent":rspP},"FM":{"Name":"LUIS ALONSO GARCIA HERNANDEZ","votes":fm,"percent":fmP},"C_PAN_PRI_PRD": {"Name":"CHRISTIAN MISHEL CASTRO BELLO","votes":pan_pri_prd,"percent":pan_pri_prdP}, "C_PAN_PRI":{"Name":"none","votes":pan_pri,"percent":pan_priP},"C_PAN_PRD":{"Name":"none","votes":pan_prd,"percent":pan_prdP}, "C_PRI_PRD": {"Name":"none","votes":pri_prd,"percent":pri_prdP}, "C_PT_MOR":{"Name":"LAYDA ELENA SANSORES SAN ROMAN","votes":pt_mor,"percent":pt_morP},"NO_REGISTRADOS": {"Name":"none","votes":no_reg,"percent":no_regP} }
-result = json.dumps(str(data))
-result = json.loads(result)
-result=result.replace("'", '"')
-print (result)
+df = df.reset_index()
+# df[cols] = df[cols].apply(pd.to_numeric, errors='coerce')
+# print (co)
+# df = pd.to_numeric(df[16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34])
+# cols = df.columns.drop(5)
+df = df.drop(['index', 0, 1,2,3,4,6,7,8,9,10,11,12,13,14,15,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46], axis=1)
+cols = df.columns.drop(5)
+df[cols] = df[cols].apply(pd.to_numeric, errors='coerce')
+df = df.groupby([5]).agg(
+    {16:'sum',
+     17:'sum',
+     18:'sum',
+     19:'sum',
+     20:'sum',
+     21:'sum',
+     22:'sum',
+     23:'sum',
+     24:'sum',
+     25:'sum',
+     26:'sum',
+     27:'sum',
+    }).reset_index()
+df = df.rename(columns={
+    5: 'MUNICIPIO',
+    16:'PAN',
+    17:'PRI',
+    18:'PRD',
+    19:'PT',
+    20:'PVEM',
+    21:'MC',
+    22:'MOR',
+    23:'PES',
+    24:'RSP',
+    25:'FM',
+    26:'CI-1',
+    27:'PAN_PRI_PRD_a',
+})
+df = df.rename(index={0: 'CALAKMUL',1: 'CALKINI',2: 'CAMPECHE',3: 'CANDELARIA',4: 'CARMEN',5: 'CHAMPOTON',6: 'DZITBALCHE',7: 'ESCARCEGA',8: 'HECELCHAKAN',9: 'HOPELCHEN',10: 'PALIZADA',11: 'SEYBAPLAYA',12: 'TENABO'})
+rc = ['PAN','PRI','PRD','PAN_PRI_PRD_a']
+df['PAN_PRI_PRD'] = df[rc].sum(axis=1)
+df = df.drop(['MUNICIPIO','PAN','PRI','PRD','PAN_PRI_PRD_a'], axis=1)
+result = df.to_json(orient="index")
+parsed = json.loads(result)
+a = json.dumps(parsed, indent=4)
+print (a)
